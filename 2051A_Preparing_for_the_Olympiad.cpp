bed9ea5bf746b31334bbcaddaa336d5c23a9b8f5
SweetBunny123 <your_new_email@example.com>
2024-12-22 22:00:08 +0530

    div3

---
+++ 2051A_Preparing_for_the_Olympiad.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n), b(n);
+        int ans = 0;
+        for(int i = 0; i < n; i++) cin >> a[i];
+        for(int i = 0; i < n; i++) cin >> b[i];
+        for(int i = 0 ;i<n-1; i++){
+            if(a[i]>b[i+1])
+            {
+                ans+= a[i]-b[i+1];
+            }
+        }
+        ans+=a[n-1];
+        cout<<ans<<endl;
+    }
+    return 0;
+}
