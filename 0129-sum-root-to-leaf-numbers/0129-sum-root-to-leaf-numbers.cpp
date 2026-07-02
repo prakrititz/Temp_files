cb16bbd1fbce6d02ea94c2c96ab648d4810bb68a
Prakrititz Borah <143780392+prakrititz@users.noreply.github.com>
2026-07-03 02:12:35 +0530

    Time: 0 ms (100%), Space: 11.7 MB (90.51%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,25 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int sumNumbers(TreeNode* root) {
-        return inOrder(root, 0);
-    }
-    
-    int inOrder(TreeNode* node, int sum) {
-        if (node == nullptr) return 0; // Base case: null node
-        
-        // Calculate the sum formed by the path from the root to the current node
-        sum = sum * 10 + node->val;
-        
-        // If the current node is a leaf node, return the sum
-        if (node->left == nullptr && node->right == nullptr) {
-            return sum;
+    int total = 0;
+    void dfs(TreeNode* root, int curr)
+    {
+        if(root== NULL){
+            return;
         }
-        
-        // Recursively calculate the sum for left and right subtrees
-        int leftSum = inOrder(node->left, sum);
-        int rightSum = inOrder(node->right, sum);
-        
-        // Return the sum of sums from left and right subtrees
-        return leftSum + rightSum;
+        int x  = curr*10+ root->val;
+        if(root->left == NULL && root->right== NULL)total += x;
+        dfs(root->left, x);
+        dfs(root->right, x); 
+    }
+    int sumNumbers(TreeNode* root) {
+        dfs(root, 0);
+        return total;
     }
 };
