9bdad6a025f824dabf0490b7c1deb1e9dd7f9bd2
Prakrititz Borah <143780392+prakrititz@users.noreply.github.com>
2026-07-13 18:29:45 +0530

    Time: 26 ms (65.07%), Space: 27.6 MB (30.93%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -15,6 +15,27 @@ public:
                 }
             }
         }
+        string res ="";
+        int i = s1.size();
+        int j = s2.size();
+        while(i>0 && j>0)
+        {
+            if(s1[i-1]==s2[j-1])
+            {
+                res += s1[i-1];
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1])
+            {
+                i--;
+            }
+            else{
+                j--;
+            }
+        }
+        reverse(res.begin(), res.end());
+        cout<<res<<endl;
         return dp[s1.size()][s2.size()];
     }
 };
